1f725c13a8e4233e9f0be6388bf3f077695eb46a
Angular CLI <angular-cli@angular.io>
2017-06-07 09:35:25 +0200

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,16 @@
+import { AngularspacePage } from './app.po';
+
+describe('angularspace App', () => {
+  let page: AngularspacePage;
+
+  beforeEach(() => {
+    page = new AngularspacePage();
+  });
+
+  it('should display welcome message', done => {
+    page.navigateTo();
+    page.getParagraphText()
+      .then(msg => expect(msg).toEqual('Welcome to app!!'))
+      .then(done, done.fail);
+  });
+});
